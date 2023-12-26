8c89a0813749fa553dcfec7860d4ba03acb38f4c
jdub <jessicadubina@Austins-MBP-3.local>
2023-12-26 15:33:53 -0800

    added auto-toggle between quiz questions box and quiz done box when the time reaches 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 //make timer: start at 1 minute, then run down to 0
 let timerElement = document.querySelector(".timer");
+let quizQuestionsElement = document.querySelector(".quiz-questions");
+let quizDoneElement = document.querySelector(".quiz-done");
 
 let timerCount = 5;
 
@@ -11,7 +13,8 @@ timer = setInterval(function() {
     //stop timer at 0
     if (timerCount === 0) {
         clearInterval(timer);
-        
+        quizQuestionsElement.setAttribute("class", "quiz-questions");
+        quizDoneElement.setAttribute("class", "active");
         //***** add code to swap .active to .quiz done ****
 
     }
