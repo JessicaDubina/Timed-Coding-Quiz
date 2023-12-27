0ccb4d25f5817f15120f9d319c036e867ee3ff39
jdub <jessicadubina@Austins-MBP-3.local>
2023-12-26 16:27:50 -0800

    added function to submit button that toggles away from quiz screen and to highscore screen. added css to hide quiz screen when not active.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@
 let timerElement = document.querySelector(".timer");
 let quizQuestionsElement = document.querySelector(".quiz-questions");
 let quizDoneElement = document.querySelector(".quiz-done");
+let submitButton = document.querySelector("#submit-initials");
+let quizScreenElement = document.querySelector("#quiz-screen");
+let headerElement = document.querySelector("header");
+let highscoreScreenElement = document.querySelector("#highscore-screen");
 
 let timerCount = 5;
 
@@ -13,12 +17,28 @@ timer = setInterval(function() {
     //stop timer at 0
     if (timerCount === 0) {
         clearInterval(timer);
+        //when timer is 0: .quiz-questions has .active removed; quiz-done has .active added
         quizQuestionsElement.setAttribute("class", "quiz-questions");
-        quizDoneElement.setAttribute("class", "active");
-        //***** add code to swap .active to .quiz done ****
-
+        quizDoneElement.setAttribute("class", "quiz-done active");
     }
 }, 1000);
 
+//function to toggle highscore and quiz screens
+const toggleScreen = () => {
+    //if quiz screen is active
+    debugger
+    if(quizScreenElement.classList.contains("active")) {
+        quizScreenElement.setAttribute("class", "quiz-screen");
+        headerElement.setAttribute("class", "quiz-screen");
+        highscoreScreenElement.setAttribute("class", "highscore-screen active")
+    } else {
+        quizScreenElement.setAttribute("class", "quiz-screen active");
+        headerElement.setAttribute("class", "quiz-screen active");
+        highscoreScreenElement.setAttribute("class", "highscore-screen")
+    }
+}
+
+//add event listener to submit button to toggle over to highscore screen
+submitButton.addEventListener("click", toggleScreen);
+
 //initial status: quiz-questions is active
-//when timer is 0: .quiz-questions has .active removed; quiz-done has .active added
