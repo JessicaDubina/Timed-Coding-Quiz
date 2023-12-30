fec343230732cedfc88b83fe78340f80e31e70e5
jdub <jessicadubina@Austins-MBP-3.local>
2023-12-30 11:36:50 -0800

    added in score accumulator and set quiz-done screen to display final score. Highscore screen also displays accumulated score. Score resets at each new test start.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,13 +14,19 @@ let startQuizButton = document.querySelector("#start-quiz");
 let backButton = document.querySelector("#back-button");
 let viewHighscoresLink = document.querySelector("#view-highscores");
 let question = document.querySelector(".question");
+let answerList = document.querySelector(".answers");
 let answers = document.querySelectorAll(".answers li");
+let quizFeedback = document.querySelector(".quiz-feedback-box").textContent;
+let finalScoreElement = document.querySelector("#final-score");
+
+let score = 0;
 
 const quizQuestions = [
     {
         question: "This is question 1",
-        options: ["option 1", "option 2", "option 3", "option 4"],
-        answer: 2
+        optionvalues: ["1. Option", "Option 2", "option 3", "option 4"],
+        optionkeys: [0, 1, 2, 3],
+        answer: "Option 2"
     },
 
     {
@@ -38,16 +44,35 @@ const quizQuestions = [
 
 
 const startQuiz = () => {
+    score = 0;
+    //move active status to quiz screen
     if (startQuizElement.classList.contains("active")) {
         startQuizElement.setAttribute("class", "start-quiz-screen");
         quizQuestionsElement.setAttribute("class", "quiz-questions active");
     } 
 
     //initialize first question/answer
-    question.textContent = quizQuestions[0].question;
+    
     for (let i = 0; i < answers.length; i++) {
-        answers[i].textContent = quizQuestions[0].options[i];
+        question.textContent = quizQuestions[0].question;
+        answers[i].textContent = quizQuestions[0].optionvalues[i];
     }
+
+    //event listener for selecting an answer
+    answerList.addEventListener("click", function(event) {
+        let userSelection = event.target;
+
+        if (userSelection.matches("li") === true ) {
+            console.log(userSelection);
+            if (userSelection.textContent === quizQuestions[0].answer) {
+                quizFeedback = "Correct!";
+                score = score + 10;
+            }
+            quizFeedback = "Wrong!";
+            //need to subtract time from counter
+        }
+        
+    })
 
     //initialize timer at max time
     let timerCount = 5;
@@ -57,44 +82,37 @@ const startQuiz = () => {
         timerElement.textContent = "Timer: " + timerCount;
     
     //stop timer at 0
-    if (timerCount === 0) {
+    if (timerCount <= 0) {
         clearInterval(timer);
-        //when timer is 0: .quiz-questions has .active removed; quiz-done has .active added
-        //initial status: .quiz-questions is .active
+        //when timer is 0, move to quiz-done screen
         quizQuestionsElement.setAttribute("class", "quiz-questions");
         quizDoneElement.setAttribute("class", "quiz-done active");
-    }
+        finalScoreElement.textContent = "Your final score is " + score;
+    } 
     }, 1000);
 }
 
 //event listener on start quiz button to switch screens and begin quiz
 startQuizButton.addEventListener("click", startQuiz);
 
-//function to toggle highscore and quiz screens
-const toggleScreen = () => {
-    //if quiz screen is active
-    if(quizScreenElement.classList.contains("active")) {
-        quizScreenElement.setAttribute("class", "quiz-screen");
-        headerElement.setAttribute("class", "quiz-screen");
-        highscoreScreenElement.setAttribute("class", "highscore-screen active")
-        addHighscores();
-    } else {
-        quizScreenElement.setAttribute("class", "quiz-screen active");
-        headerElement.setAttribute("class", "quiz-screen active");
-        highscoreScreenElement.setAttribute("class", "highscore-screen")
-    }
+//function to toggle to highscore screen
+const toggleScreenToHighscores = () => {
+    quizScreenElement.setAttribute("class", "quiz-screen");
+    headerElement.setAttribute("class", "quiz-screen");
+    highscoreScreenElement.setAttribute("class", "highscore-screen active")
+    addHighscores();
 }
 
 //add event listener to submit button to toggle over to highscore screen
 submitButton.addEventListener("click", function() {
     let userHighscores = {
         user: userInitialsInput.value.trim(),
-        score: "0"
+        score: score
     };
 
     localStorage.setItem("userHighscores",JSON.stringify(userHighscores));
     userInitialsInput.value = "";
-    toggleScreen();
+    toggleScreenToHighscores();
 });
 
 //take new user highscore form storage and append to highscore list
